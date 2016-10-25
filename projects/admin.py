86905785b3e9779d8282453147c2da3e6fc1071a
sylvan1 <spaczusk@gmail.com>
2016-10-25 18:41:03 +0200

    Code review.

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from projects.models import Project
 
+
 @admin.register(Project)
 class ProjectsAdmin(admin.ModelAdmin):
-	exclude = ('slug',)
+    exclude = ('slug',)
 
